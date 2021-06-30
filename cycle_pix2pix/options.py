9a8bb2672874f3a3d26e1874786bd41e074ecce2
sysu19351120 <61102174+sysu19351120@users.noreply.github.com>
2021-07-01 06:32:59 +0800

    Add files via upload

---
+++ cycle_pix2pix/options.py
@@ -0,0 +1,43 @@
+import argparse
+import os
+import torch
+
+class TrainOptions():
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.initialized = False
+
+    def initialize(self):
+        self.parser.add_argument('--save_dir', type=str, default="save_dir", help='the name of the experiment')
+        self.parser.add_argument('--epoch_start', type=int, default=0, help='epoch to start training from')
+        self.parser.add_argument('--epoch_num', type=int, default=200, help='number of epochs of training')
+        self.parser.add_argument('--data_root', type=str, default='./data/', help='dir of the dataset')
+        self.parser.add_argument('--dataset_name', type=str, default="facades", help='name of the dataset')
+        self.parser.add_argument('--batch_size', type=int, default=4, help='size of the batches')
+        self.parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate')
+        self.parser.add_argument('--b1', type=float, default=0.5, help='adam: decay of first order momentum of gradient')
+        self.parser.add_argument('--b2', type=float, default=0.999, help='adam: decay of first order momentum of gradient')
+        self.parser.add_argument('--decay_epoch', type=int, default=100, help='epoch from which to start lr decay')
+        self.parser.add_argument('--n_cpu', type=int, default=0, help='number of cpu threads to use during batch generation')
+        self.parser.add_argument('--img_height', type=int, default=256, help='size of image height')
+        self.parser.add_argument('--img_width', type=int, default=256, help='size of image width')
+        self.parser.add_argument('--in_channels', type=int, default=3, help='number of input image channels')
+        self.parser.add_argument('--out_channels', type=int, default=3, help='number of output image channels')
+        self.parser.add_argument('--sample_interval', type=int, default=200, help='interval between sampling of images from generators')
+        self.parser.add_argument('--checkpoint_interval', type=int, default=-1, help='interval between model checkpoints')
+        self.parser.add_argument('--which_direction', type=str, default='AtoB', help='AtoB or BtoA')
+        self.parser.add_argument('--n_D_layers', type=int, default=3, help='used to decision the patch_size in D-net, should less than 8')
+        self.parser.add_argument('--lambda_pixel', type=int, default=100, help=' Loss weight of L1 pixel-wise loss between translated image and real image')
+        self.parser.add_argument('--img_result_dir', type=str, default='result_images', help=' where to save the result images')
+        self.parser.add_argument('--model_result_dir', type=str, default='saved_net', help=' where to save the checkpoints')
+
+
+    def parse(self):
+        if not self.initialized:
+            self.initialize()
+        args = self.parser.parse_args()
+
+        os.makedirs('%s/%s' % (args.save_dir, args.img_result_dir), exist_ok=True)
+        os.makedirs('%s/%s' % (args.save_dir, args.model_result_dir), exist_ok=True)
+        self.args = args
+        return self.args
---
+++ cycle_pix2pix/utils.py
@@ -0,0 +1,41 @@
+import torch
+from torch.autograd import Variable
+from torchvision.utils import save_image
+
+def sample_images(generator, test_dataloader, args, epoch,i):
+    """用于保存img"""
+    imgs = next(iter(test_dataloader))
+    if torch.cuda.is_available():
+        real_A = Variable(imgs['A'].type(torch.FloatTensor).cuda())
+        real_B = Variable(imgs['B'].type(torch.FloatTensor).cuda())
+    else:
+        real_A = Variable(imgs['A'].type(torch.FloatTensor))
+        real_B = Variable(imgs['B'].type(torch.FloatTensor))
+    fake_B = generator(real_A)
+    #按照从上到下的顺序拼接图片
+    img_sample = torch.cat((real_A.data, fake_B.data, real_B.data), -2)
+    save_image(img_sample, '%s/%s/%s_%s.png' % (args.save_dir, args.img_result_dir,  epoch,i), nrow=5, normalize=True)
+class ReplayBuffer:
+    """
+    缓存队列，若不足则新增，否则随机替换
+    """
+
+    def __init__(self, max_size=50):
+        self.max_size = max_size
+        self.data = []
+
+    def push_and_pop(self, data):
+        to_return = []
+        for element in data.data:
+            element = torch.unsqueeze(element, 0)
+            if len(self.data) < self.max_size:
+                self.data.append(element)
+                to_return.append(element)
+            else:
+                if random.uniform(0, 1) > 0.5:
+                    i = random.randint(0, self.max_size - 1)
+                    to_return.append(self.data[i].clone())
+                    self.data[i] = element
+                else:
+                    to_return.append(element)
+        return Variable(torch.cat(to_return))
